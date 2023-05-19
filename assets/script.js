738cbacebd5789f6ad012f560e3b56002764d435
Drew Jordan <ajordan12@luc.edu>
2023-05-19 18:49:08 -0500

    started js

---
+++ assets/script.js
@@ -0,0 +1,51 @@
+// quiz questions and answers ---> add however many questions within const quizData
+const quizData = [
+    // question 1
+    {
+        question: "",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+    },
+    // question 2
+    {
+        question: "",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+    },
+    // question 3
+    {
+        question: "",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+    },
+    // question 4
+    {
+        question: "",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+    },
+    // question 5
+    {
+        question: "",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+    },
+    // question 6
+    {
+        question: "",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+    },
+]
